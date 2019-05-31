a892e77d5bc10922c7b2aced408d4e686c5631b0
udaranasinghe <50186086+udaranasinghe@users.noreply.github.com>
2019-05-31 20:36:16 +1000

    Update myscript.sh
    In practical eight, msbar tool is used to incoorporate mismatches in in the provided DNA sequence(sample 09). so, the important data such as, length of the sequence, e-value, number of mismatches and bit score can be anlised within a very short time. using this tool is really important in extract a specific genetic information from the huge data set and to do the analyse. the ability to save the codes in GitHub also helps to use the knowledge for future studies.

--- myscript.sh
+++ myscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "hello world"
-
+#make a blast index of CDS sequences 
 formatdb -p F -o T -i Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa
 
 msbar -sequence sample.fa -count 100 -point 4 -block 0 -codon 0 -outseq sample_mutated.fa
