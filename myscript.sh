e2292e4f9c057377b9a556ae97598680ca6a4c82
udaranasinghe <50186086+udaranasinghe@users.noreply.github.com>
2019-05-31 11:29:21 +1000

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,5 +1,7 @@
+
 #!/bin/bash
 echo "hello world"
+# given below are the codes for change the mutation count for mutate blast for the sample 09 the count was change by 50 increments.
 
 formatdb -p F -o T -i Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa
 
