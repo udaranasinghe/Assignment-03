9545da4a94eae7914d289f0d0158724dba93b2ef
udaranasinghe <50186086+udaranasinghe@users.noreply.github.com>
2019-05-31 14:19:56 +1000

    Update myscript.sh
    here the msbar tool is used to incorporate mismatches in the DNA sequence provided(sample 09). So, the limitations of the blast in detecting mismatches can also be detected by providing this command line. the value of is changed in 50 increments the command was run to assess the change of bit score , evalue and the number of mismatches.

--- myscript.sh
+++ myscript.sh
@@ -2,10 +2,10 @@
 #!/bin/bash
 echo "hello world"
 # given below are the codes for change the mutation count for mutate blast for the sample 09 the count was change by 50 increments.
-
+#make a blast index of CDS sequences .
 formatdb -p F -o T -i Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa
 
 msbar -sequence sample.fa -count 100 -point 4 -block 0 -codon 0 -outseq sample_mutated.fa
-
+#tabular results
 blast2 -p blastn -e 0.001 -m 8 -d Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa -i sample_mutated.fa
 
